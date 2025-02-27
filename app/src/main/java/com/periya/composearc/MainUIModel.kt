fd81bd160155b40deb13f4bbabb63f576f08b0cc
Periya <periyamydell@gmail.com>
2025-02-27 21:27:56 +0530

    Revert "init commit"
    This reverts commit 870ab357d19a70e19ac04fcfce97f04a5065bb73.

--- app/src/main/java/com/periya/composearc/MainUIModel.kt
+++
@@ -1,7 +0,0 @@
-package com.periya.composearc
-
-data class MainUIModel(
-    val productId: String = EMPTY,
-    val title: String = EMPTY,
-    val userList : List<String> = emptyList(),
-    val onLoadClick : (() -> Unit)? = null)
